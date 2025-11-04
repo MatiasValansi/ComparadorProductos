52de1ef9d88a6310418a978d296575b65b16a3b7
MatiasValansi <valansimatias@gmail.com>
2025-11-04 14:18:19 -0300

    Se modifico la clase para poder buscar el elemento que se recibe como parametro

--- src/api/mercadoLibre.js
+++ src/api/mercadoLibre.js
@@ -1,23 +1,28 @@
+// src/api/mercadoLibre.js
 import axios from 'axios';
 
-const options = {
-  method: 'GET',
-  url: 'https://mercado-libre8.p.rapidapi.com/search',
-  params: {
-    keyword: 'laptop',
-    country: 'ar',
-    page: '2',
-    sort: 'price_low_to_high'
-  },
-  headers: {
-    'x-rapidapi-key': '14b3c4584amshc969a09f0aaba19p1f0b6cjsnef9e38d2b7ec',
-    'x-rapidapi-host': 'mercado-libre8.p.rapidapi.com'
-  }
-};
+export async function searchMercadoLibre(query) {
+  const options = {
+    method: 'GET',
+    url: 'https://mercado-libre8.p.rapidapi.com/search',
+    params: {
+      keyword: query,   
+      country: 'ar',
+      page: '1',
+      sort: 'relevance'
+    },
+    headers: {
+      'x-rapidapi-key': import.meta.env.X_RAPID_KEY_RAPID_API || '14b3c4584amshc969a09f0aaba19p1f0b6cjsnef9e38d2b7ec',
+      'x-rapidapi-host': 'mercado-libre8.p.rapidapi.com'
+    }
+  };
 
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
+  try {
+    const response = await axios.request(options);
+    console.log('Datos obtenidos:', response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Error buscando en Mercado Libre:', error);
+    return { results: [] };
+  }
 }
