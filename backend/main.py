1d9e0f74fb96b182525f385d99d7afd4b77c7969
MatiasValansi <valansimatias@gmail.com>
2025-11-06 15:50:06 -0300

    Se realizaron los métodos HTTP correspondientes a nuestro CRUD

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,27 @@ app = FastAPI()
 @app.get("/")
 def test():
     return {"message": "Testeando Backend!"}
+
+@app.get("/api/favourites/{fav_id}")
+async def get_fav_by_id(fav_id: int):
+    return {"fav_id": fav_id}
+
+@app.get("api/favourites")
+async def get_all_favs():
+    return {"favourites": []}
+
+@app.post("/api/favourites/{fav_id}")
+async def create_fav(fav_id: int):
+    return {"fav_id": fav_id}
+
+@app.put("/api/favourites/{fav_id}")
+async def update_fav(fav_id: int):
+    return {"fav_id": fav_id}
+
+@app.delete("/api/favourites/{fav_id}")
+async def delete_fav(fav_id: int):
+    return {"fav_id": fav_id}
+
+@app.delete("/api/favourites")
+async def delete_all_favs():    
+    return {"message": "All favourites deleted"}
