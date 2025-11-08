c9884e8743cfbe4371478a8054c9233ca3f447f6
MatiasValansi <valansimatias@gmail.com>
2025-11-08 20:22:48 -0300

    Se agregaron buenas practicas y validaciones con FastAPI

--- backend/database.py
+++ backend/database.py
@@ -19,7 +19,7 @@ database = client.favdatabase
 favourites_collection = database.favorites
 
 #Realizo los métodos de la BD
-async def get_one_fav_by_id(favorite_id):
+async def get_one_fav_by_id(favorite_id:int):
     fav_found = await favourites_collection.find_one({"_id": favorite_id})
     return fav_found
 
@@ -30,16 +30,16 @@ async def get_all_favs():
         all_favs.append(FavouriteProduct(document))
     return all_favs
 
-async def create_fav(fav_to_create):
-    new_fav = await favourites_collection.insert_one(fav_to_create)
+async def create_fav(fav_to_create:FavouriteProduct):
+    new_fav = await favourites_collection.insert_one(fav_to_create.dict())
     fav_created = await favourites_collection.find_one({"_id": new_fav.inserted_id})
     return fav_created
 
-async def update_fav(favorite_id, fav_to_update):
+async def update_fav(favorite_id:int, fav_to_update:FavouriteProduct):
     await favourites_collection.update_one({"_id": favorite_id}, {"$set": fav_to_update})
     fav_updated = await favourites_collection.find_one({"_id": favorite_id})
     return fav_updated
 
-async def delete_fav(favorite_id):
+async def delete_fav(favorite_id:int):
     fav_deleted = await favourites_collection.delete_one({"_id": favorite_id})
     return fav_deleted
--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 
+from favourite_product import FavouriteProduct
+
 app = FastAPI()
 
 @app.get("/")
 def test():
     return {"message": "Testeando Backend!"}
 
-@app.get("/api/favourites/{fav_id}")
+@app.get("/api/favourites/")
 async def get_fav_by_id(fav_id: int):
     return {"fav_id": fav_id}
 
@@ -14,15 +16,15 @@ async def get_fav_by_id(fav_id: int):
 async def get_all_favs():
     return {"favourites": []}
 
-@app.post("/api/favourites/{fav_id}")
-async def create_fav(fav_id: int):
-    return {"fav_id": fav_id}
+@app.post("/api/favourites/")
+async def create_fav(fav_to_create: FavouriteProduct):
+    return {"fav_id": fav_to_create}
 
-@app.put("/api/favourites/{fav_id}")
-async def update_fav(fav_id: int):
-    return {"fav_id": fav_id}
+@app.put("/api/favourites/")
+async def update_fav(fav_id: int, fav_to_update: FavouriteProduct):
+    return {"fav_id": fav_id, "fav_updated": fav_to_update}
 
-@app.delete("/api/favourites/{fav_id}")
+@app.delete("/api/favourites/")
 async def delete_fav(fav_id: int):
     return {"fav_id": fav_id}
 
